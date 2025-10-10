61288263927e43af741f149091a4f16ef70f57dd
kirill235-22 <kablov2006@gmail.com>
2025-10-10 10:05:29 +0700

    Добавлен класс Square

--- lab2.cpp
+++ lab2.cpp
@@ -245,6 +245,179 @@ class Rook: public Piece{
         }
 };
 
+class Square{
+    private:
+        Piece* _piece; //фигура на клетке
+        coordinates _pos; //координаты клетки
+
+    public:
+        Square(coordinates pos): _pos(pos) ,_piece(nullptr){};
+        Square(coordinates pos, pieceType type, color col): _pos(pos){
+            switch (type)
+            {
+            case pieceType::bishop:
+                setPiece(new Bishop(getPos(), col));
+                break;
+            
+            case pieceType::king:
+                setPiece(new King(getPos(), col));
+                break;
+
+            case pieceType::knight:
+                setPiece(new Knight(getPos(), col));
+                break;
+
+            case pieceType::pawn:
+                setPiece(new Pawn(getPos(), col));
+                break;
+
+            case pieceType::queen:
+                setPiece(new Queen(getPos(), col));
+                break;
+
+            case pieceType::rook:
+                setPiece(new Rook(getPos(), col));
+                break;
+
+            default:
+                removePiece();
+                break;
+            }
+        }
+
+        //получить координаты клетки
+        coordinates getPos(){
+            return _pos;
+        }
+
+        //поменять фигуру (при срубе или в конечной клетке)
+        void setPiece(Piece* piece){
+            if (_piece!=nullptr) _piece->setDead();
+            _piece = piece;
+            _piece->setPos(getPos());
+        }
+
+        //какая фигура стоит на клетке
+        Piece* getPiece(){
+            return _piece;
+        }
+
+        //убрать фигуру (при обычном ходе в начальной клетке)
+        void removePiece(){
+            _piece = nullptr;
+        }
+
+        color getPieceColor(){
+            return _piece->getColor();
+        }
+
+        pieceType getPieceType(){
+            return _piece->getType();
+        }
+
+        vector<coordinates> getPiecePattern(){
+            return _piece->getPattern();
+        }
+
+        bool hasPiece(){
+            if (getPiece()!=nullptr) return true;
+            return false;
+        }
+
+        //получить доступные для фигуры на клетке ходы
+        vector<coordinates> getMoves(Square* board[8][8], color active){
+            vector<coordinates> moves;
+            switch (board[getPos().getX()][getPos().getY()]->getPieceType()){
+                case pieceType::bishop:
+                case pieceType::rook:
+                case pieceType::queen:
+                    moves = getContMoves(board, active);
+                    break;
+
+                case pieceType::king:
+                    moves = getShortMoves(board, active);
+                    break;
+
+                case pieceType::knight:
+                    moves = getShortMoves(board, active);
+                    break;
+
+                case pieceType::pawn:
+                    moves = getPawnMoves(board, active);
+                    break;
+            }
+            return moves;
+        }
+
+        vector<coordinates> getContMoves(Square* board[8][8], color active){
+            vector<coordinates> moves, pattern;
+            coordinates pos = getPos();
+            pattern = getPiecePattern();
+            for (coordinates move : pattern){
+                int n=1;
+                bool stop=false;
+                while (!stop){
+                    coordinates tpos = pos + move*n;
+                    if (tpos.checkBound()){
+                        if (!board[tpos.getX()][tpos.getY()]->hasPiece()){
+                            moves.push_back(tpos);
+                            n++;
+                        }
+                        else if (board[tpos.getX()][tpos.getY()]->getPieceColor()!=active){
+                            moves.push_back(tpos);
+                            stop = true;
+                        }
+                        else stop = true;
+                    }
+                    else stop=true;
+                }
+            }
+            return moves;
+        }
+
+        vector<coordinates> getShortMoves(Square* board[8][8], color active){
+            vector<coordinates> moves, pattern;
+            coordinates pos = getPos();
+            pattern = getPiecePattern();
+            for (coordinates move : pattern){
+                coordinates tpos = pos + move;
+                    if (tpos.checkBound()){
+                        if (!board[tpos.getX()][tpos.getY()]->hasPiece() || board[tpos.getX()][tpos.getY()]->getPieceColor()!=active)
+                            moves.push_back(tpos);
+                    }
+            }
+            return moves;
+        }
+        
+        vector<coordinates> getPawnMoves(Square* board[8][8], color active){
+            vector<coordinates> moves, pattern;
+            coordinates pos = getPos(), tpos, forward;
+            pattern = getPiecePattern();
+            for (coordinates move : pattern){
+                if (getPieceColor()==color::white) {
+                    tpos = pos + move;
+                    forward = pos + coordinates(0, 1);
+                }
+                else {
+                    tpos = pos - move;
+                    forward = pos - coordinates(0, 1);
+                }
+                if (tpos.checkBound()){
+                    if (move.getX()==0){
+                        if (!board[forward.getX()][forward.getY()]->hasPiece())
+                            moves.push_back(tpos);
+                    }
+                    else{
+                        if (board[tpos.getX()][tpos.getY()]->hasPiece() && board[tpos.getX()][tpos.getY()]->getPieceColor()!=active)
+                            moves.push_back(tpos);
+                    }
+                }
+            }
+            return moves;
+        }
+
+};
+
 int main(){
     Piece *pawn = new Pawn();
     pawn->printStats();
